d71d3ddcdb1417da187069a602c677fd39ada184
Robby McKilliam <harprobey@gmail.com>
2007-04-30 22:29:52 +0000

    changes, to the BasisScrableEstimator.  This now needs major modification to actually work.

--- src/simulator/BasisScrambleEstimator.java
+++ src/simulator/BasisScrambleEstimator.java
@@ -9,98 +9,128 @@ package simulator;
 /**
  * Keeps moving along the basis vectors  that are closest to the
  * search line.  Guaranteed ML.  Something like o(n^2.5) time.
+ * Unfortunately, I now realise that this must actually add a
+ * relavant vector, not just a basis vector such that it is closest
+ * to the line zeta.  I am not sure how to do this efficiently yet.
  * @author Robby McKilliam
  */
 public class BasisScrambleEstimator extends Anstar implements PRIEstimator {
     
-    double[] zeta, fzeta, zetaproj, posv, negv;
+    double[] zeta, fzeta;
+    double N;
     
     public void setSize(int n){
-        setDimension(n-1);
+        N = n;
+        setDimension(n);
         zeta = new double[n];
-	fzeta = new double[n];
-        zetaproj = new double[n];
-        posv = new double[n];
-        negv = new double[n];
+        fzeta = new double[n];
     }
     
     public double estimateFreq(double[] y, double fmin, double fmax)
     {
-      
+        if(n != y.length)
+            setSize(y.length);
+        
         double ymean = VectorFunctions.mean(y);
-        for (int i = 0; i <= n; i++)
+        for (int i = 0; i < N; i++)
             zeta[i] = y[i] - ymean;
         
         double zetatzeta = 0.0;
-        for (int i = 0; i <= n; i++){
+        for (int i = 0; i < N; i++){
             zetatzeta += zeta[i]*zeta[i];
             fzeta[i] = fmin * zeta[i];
         }
         nearestPoint(fzeta);
         
-        for (int i = 0; i <= n; i++)
-            zetaproj[i] = zeta[i]*zeta[i]/zetatzeta;
-        
         double fhat = 0.0, f = 0.0, bestL = Double.POSITIVE_INFINITY;
-        double[] vp = v;
         
         while(f < fmax){
             
-            double posvproj = 0.0, posfproj = 0.0;
-            double negvproj = 0.0, negfproj = 0.0;
-            for (int i = 0; i <= n; i++){
-               posv[i] = vp[i] - 1.0/(n+1.0); 
-               posvproj += Math.pow(v[i] - vp[i]*zetaproj[i],2);
-               posfproj += v[i]*zeta[i]/zetatzeta;
-               negv[i] = vp[i] + 1.0/n;
-               negvproj += Math.pow(v[i] - vp[i]*zetaproj[i],2);
-               negfproj += vp[i]*zeta[i]/zetatzeta;
+            
+            double pvproj = 0.0, pfproj = 0.0;
+            for (int i = 0; i < N; i++){
+               v[i] -= 1.0/N; 
+               pvproj += Math.pow(v[i] - v[i]*zeta[i]*zeta[i]/zetatzeta, 2);
+               pfproj += v[i]*zeta[i];
             }
+            pfproj /= zetatzeta;
             
-            System.out.println(VectorFunctions.print(vp));
-            
-            double bproj, L = Double.POSITIVE_INFINITY, f0, fold = f;
+            double bproj = 0.0, L = Double.POSITIVE_INFINITY, f0, fold = f;
             int besti = 0;
             boolean bestIsPos = true;
-            for(int i = 0; i <= n; i++){
-                bproj = posvproj + Math.pow(1 -  zetaproj[i], 2)*
-                        ( Math.pow( posv[i] + 1 - 1.0/(n+1.0) , 2) 
-                        - Math.pow( posv[i] - 1.0/(n+1.0) , 2) );
-                f0 = posfproj + zeta[i]/zetatzeta;
+            for(int i = 0; i < N; i++){
+                bproj = pvproj + Math.pow(1 -  zeta[i]*zeta[i]/zetatzeta, 2)*
+                        ( Math.pow( v[i] + 1.0 - 1.0/N , 2) 
+                        - Math.pow( v[i] - 1.0/N , 2) );
+                f0 = pfproj + zeta[i]/zetatzeta;
+                bproj /= (f0*f0);
                 if(bproj < L && f0 > fold){
                     L = bproj;
                     besti = i;
                     f = f0;
                 }
-                bproj = negvproj + Math.pow(1 -  zetaproj[i], 2)*
-                        ( Math.pow( negv[i] - 1 + 1.0/(n+1.0) , 2) 
-                        - Math.pow( negv[i] + 1.0/(n+1.0) , 2) );
-                f0 = negfproj + zeta[i]/zetatzeta;
-                if(bproj < L && f0 > fold){
-                    L = bproj;
-                    besti = i;
-                    f = f0;
-                    bestIsPos = false;
-                } 
             }
             
-            //make v the next lattice point along the line
-            if(bestIsPos){
-                vp = posv;
-                vp[besti] += 1.0;
-            }else{
-                vp = negv;
-                vp[besti] -= 1.0;
+            
+            /*
+            //slow way
+            double L=Double.POSITIVE_INFINITY, Li, fold = f, f0;
+            int besti = 0;
+            boolean negbasis = false;
+            for(int i = 0; i < N; i++){
+                v[i] += 1.0;
+                Li = 0.0; 
+                f0 = 0.0;
+                for(int k = 0; k < N; k++){
+                    Li += Math.pow(v[k] - 1.0/N - (v[k] - 1.0/N)*zeta[k]*zeta[k]/zetatzeta, 2); 
+                    f0 += (v[k] - 1.0/N)*zeta[k]/zetatzeta;
+                }
+                if(Li < L && f0 > fold){
+                    L = Li;
+                    f = f0;
+                    besti = i;
+                }
+                System.out.println(Li + "\t" + L + "\t" + i + "\t" + besti + "\t" + f0 + "\t" + negbasis);
+                v[i] -= 2.0;
+                Li = 0.0;
+                f0 = 0.0;
+                for(int k = 0; k < N; k++){
+                    Li += Math.pow(v[k] + 1.0/N - (v[k] + 1.0/N)*zeta[k]*zeta[k]/zetatzeta, 2);
+                    f0 += (v[k] + 1.0/N)*zeta[k]/zetatzeta;
+                }
+                if(Li < L && f0 < fold){
+                    L = Li;
+                    besti = i;
+                    f = f0;
+                    negbasis = true;
+                }
+                System.out.println(Li + "\t" + L + "\t" + i + "\t" + besti + "\t" + f0 + "\t" + negbasis);
             }
+             */
+            
+            //make v the next lattice point along the line
+            if(!negbasis){
+                for(int i = 0; i < N; i++)
+                    v[i] -= 1.0/N;
+                v[besti] += 1.0;
+            }
+            else{
+                for(int i = 0; i < N; i++)
+                    v[i] += 1.0/N;
+                v[besti] -= 1.0;
+            }
+            
+            //System.out.println("1 " + L + "\t" + bestL + "\t" + f + "\t" + fhat);
             
             //if the current estimate is the best, then calculate
             //the value of fhat corresponding to the current x
-            if(L < bestL){
+            if(L < bestL && f > fmin){
                 bestL = L;
                 fhat = f;
             }
             
-            System.out.println(L);
+            System.out.println("2 " + L + "\t" + bestL + "\t" + f + "\t" + fhat);
+            
             //System.out.println(VectorFunctions.print(u));
                  
         }
