76e8cb87e121ae7d690ebde7d60e4d975e68bf62
Robby McKilliam <harprobey@gmail.com>
2007-06-20 07:04:52 +0000

    Added the AnLLS search code.  This has some errors and needs a bit of a rethink.

---
+++ src/simulator/AnLLS.java
@@ -0,0 +1,178 @@
+/*
+ * AnLLS.java
+ *
+ * Created on 20 June 2007, 13:39
+ */
+
+package simulator;
+
+/**
+ * This is similar to the bresenham estimator but
+ * it uses relationships Zn->An->An* to search
+ * each point.  This should be slower than the
+ * bresenham estimator, expected to require atmost
+ * O(n^2 sqrt(n)) steps with each step requiring O(n) 
+ * calculations.  Total O(n^3 sqrt(n)).
+ * @author Robby McKilliam
+ */
+public class AnLLS implements PRIEstimator {
+    
+    protected double[] kappa, v, g;
+    double n;
+    
+    /** Creates a new instance of AnLLS */
+    public AnLLS() {
+    }
+    
+    /** 
+     * Sets protected variable g to the glue
+     * vector [i].  See SLAG pp109.
+     */
+    protected void glueVector(double i){
+        double j = n + 1 - i;
+        for(int k = 0; k < j; k++)
+            g[k] = i/(n+1);
+        for(int k = (int)j; k < n + 1; k++)
+            g[k] = -j/(n+1);
+    }
+    
+    public void setSize(int N) {
+        n = N-1;
+	kappa = new double[N];
+        v = new double[N];
+        g = new double[N];
+    }
+    
+    public double estimateFreq(double[] y, double fmin, double fmax){
+        if (n != y.length-1)
+	    setSize(y.length);
+        
+        Anstar.project(y, y);
+        
+        double bestf = 0.0;
+        double bestdist2 = Double.POSITIVE_INFINITY;
+        double f;
+        
+        for(int i = 0; i <= n; i++){
+            glueVector(i);
+            f = fmin;
+            
+                    int count = 0;
+            
+            while(f < fmax){
+                
+                for(int j = 0; j <= n; j++){
+                    if(y[j]>0.0)
+                        v[j] = pround(f*y[j] + g[j]);
+                    else
+                        v[j] = nround(f*y[j] + g[j]);
+                }
+                System.out.println(VectorFunctions.print(v));
+                
+                //using the lattice point update f
+                //theoretically this should only be
+                //necessary when we sum(v)=0 (ie this
+                //is a point in An) but to avoid
+                //numerically error with round we have
+                //to do it at every point in Zn.
+                double ytv = 0.0, yty = 0.0;
+                for(int j = 0; j <= n; j++){
+                    ytv += y[j]*v[j];
+                    yty += y[j]*y[j];
+                }
+                double f0 = ytv/yty;
+                f = Math.max(fmin, f0);
+                
+                //if sum(v) is zero this is a lattice point
+                //in An so calculate its distance from the 
+                //line y
+                if(VectorFunctions.sum(v) == 0.0){
+                    double dist2 = 0.0;
+                    for(int j = 0; j <= n; j++){
+                        double diff = f*y[j] - v[j];
+                        dist2 = diff*diff;
+                    }
+                    if( dist2 < bestdist2 ){
+                        bestdist2 = dist2;
+                        bestf = f0;
+                    }      
+                }
+                
+                //calculate the next f that moves into another
+                //voronoi region in Zn
+                double mindel = Double.POSITIVE_INFINITY;
+                for(int j = 0; j <= n; j++){
+                    double del = Double.POSITIVE_INFINITY;
+                    if( y[j] > 0.0 )
+                        del = (0.5 + pround(f*y[j] + g[j]) - g[j])/y[j] - f;
+                    else if(y[j] < 0.0)
+                        del = (-0.5 + nround(f*y[j] + g[j]) - g[j])/y[j] - f;
+                    else del = Double.POSITIVE_INFINITY;
+                    
+                    System.out.println(del);
+                    
+                    mindel = Math.min(mindel, del);         
+                }
+                System.out.println("f0 = " + f0);
+                System.out.println("f = " + f);
+                if( mindel <= 0.0 )
+                    throw new Error( "mindel <= zero!\n" + mindel + "\n" + f + "\n" + VectorFunctions.print(y) + "\n" + VectorFunctions.print(g) + "\n" + VectorFunctions.print(v) );
+                
+                f += mindel;
+                System.out.println("f = " + f); 
+                System.out.println();
+             
+                if(count++ > 500)
+                    throw new Error( "too many loops" );
+                
+            }
+            System.out.println("finished glue" + i + " with bestf so far = " + bestf);
+        }
+        return bestf;
+    }
+    
+    /**
+     * Specialised round. Rounds 0.5 and -0.5 to greatest
+     * magnitude. <br>
+     * Eg:
+     * round(-1.5) = -2
+     * round(1.5) = 2
+     */
+    public static double round(double x){
+        if(x > 0)
+            return Math.floor(x + 0.5);
+        else
+            return -Math.floor(-x + 0.5);
+    }
+    
+    /**
+     * nround(1.5) = 1.0
+     * nround(-1.5) = -2.0 
+     */
+    public static double nround(double x){
+        return -Math.floor(-x + 0.5);
+    }
+    
+    /**
+     * pround(1.5) = 2.0
+     * pround(-1.5) = 1.0 
+     */
+    public static double pround(double x){
+        return Math.floor(x + 0.5);
+    }
+    
+    public double varianceBound(double sigma, double[] k) {
+	Anstar.project(k, kappa);
+	double sk = 0;
+	for (int i = 0; i < k.length; i++)
+	    sk += kappa[i] * kappa[i];
+	return sigma * sigma / sk;
+    }
+    
+    /** 
+     * Returns the glue vector g. This getter
+     * is really only here for testing purposes.
+     */
+    public double[] getg() { return g; }
+    
+}
---
+++ test/simulator/AnLLSTest.java
@@ -0,0 +1,160 @@
+/*
+ * AnLLSTest.java
+ * JUnit based test
+ *
+ * Created on 20 June 2007, 14:03
+ */
+
+package simulator;
+
+import junit.framework.*;
+
+/**
+ *
+ * @author robertm
+ */
+public class AnLLSTest extends TestCase {
+    
+    public AnLLSTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * Test of glueVector method, of class simulator.AnLLS.
+     */
+    public void testGlueVector() {
+        System.out.println("glueVector");
+        
+        AnLLS instance = new AnLLS();
+        
+        double n = 3;
+        instance.setSize((int)n+1);
+        
+        double i = 0;
+        instance.glueVector(i);
+        double[] result = instance.getg();
+        double[] expResult0 = {0.0,0.0,0.0,0.0};
+        for(int k = 0; k <= n; k++)
+            assertEquals(result[k], expResult0[k]);
+        
+        i = 1;
+        instance.glueVector(i);
+        result = instance.getg();
+        double[] expResult1 = {1/(n+1),1/(n+1),1/(n+1),-3/(n+1)};
+        for(int k = 0; k <= n; k++)
+            assertEquals(result[k], expResult1[k]);
+        
+        i = 2;
+        instance.glueVector(i);
+        result = instance.getg();
+        double[] expResult2 = {2/(n+1),2/(n+1),-2/(n+1),-2/(n+1)};
+        for(int k = 0; k <= n; k++)
+            assertEquals(result[k], expResult2[k]);
+        
+        i = 3;
+        instance.glueVector(i);
+        result = instance.getg();
+        double[] expResult3 = {3/(n+1),-1/(n+1),-1/(n+1),-1/(n+1)};
+        for(int k = 0; k <= n; k++)
+            assertEquals(result[k], expResult3[k]);
+        
+    }
+
+    /**
+     * Test of round method, of class simulator.AnLLS.
+     */
+    public void testRound() {
+        System.out.println("round");
+        
+        int N = 0;
+        AnLLS instance = new AnLLS();
+        assertEquals(-2.0, instance.round(-1.5));
+        assertEquals(-2.0, instance.round(-1.6));
+        assertEquals(-1.0, instance.round(-1.3));
+        assertEquals(2.0, instance.round(1.5));
+        assertEquals(2.0, instance.round(1.6));
+        assertEquals(1.0, instance.round(1.3));
+    }
+    
+    
+    /**
+     * Test of nround method, of class simulator.AnLLS.
+     */
+    public void testNround() {
+        System.out.println("round");
+        
+        int N = 0;
+        AnLLS instance = new AnLLS();
+        assertEquals(-2.0, instance.nround(-1.5));
+        assertEquals(-2.0, instance.nround(-1.6));
+        assertEquals(-1.0, instance.nround(-1.3));
+        assertEquals(1.0, instance.nround(1.5));
+        assertEquals(2.0, instance.nround(1.6));
+        assertEquals(1.0, instance.nround(1.3));
+    }
+    
+        /**
+     * Test of pround method, of class simulator.AnLLS.
+     */
+    public void testPround() {
+        System.out.println("round");
+        
+        int N = 0;
+        AnLLS instance = new AnLLS();
+        assertEquals(-1.0, instance.pround(-1.5));
+        assertEquals(-2.0, instance.pround(-1.6));
+        assertEquals(-1.0, instance.pround(-1.3));
+        assertEquals(2.0, instance.pround(1.5));
+        assertEquals(2.0, instance.pround(1.6));
+        assertEquals(1.0, instance.pround(1.3));
+    }
+
+    /**
+     * Test of estimateFreq method, of class simulator.AnLLS.
+     */
+    public void testEstimateFreq() {
+        System.out.println("estimateFreq");
+        
+        double[] y = {1.0,2.0,3.0,4.0,5.0};
+        double fmin = 0.1;
+        double fmax = 1.9;
+        int n = 5;
+        double T = 1.0;
+        AnLLS instance = new AnLLS();
+        
+        /*
+        GaussianNoise noise = new simulator.GaussianNoise(0.0,0.0000001*0.0000001);
+        
+        SparseNoisyPeriodicSignal sig = new SparseNoisyPeriodicSignal();
+        sig.setPeriod(T);
+        sig.setNoise(noise);
+          
+        noise.setSeed(200);
+        sig.generateTransmittedSignal(n,200);
+        */
+        
+        double expResult = 1.0;
+        double result = instance.estimateFreq(y, fmin, fmax);
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of varianceBound method, of class simulator.AnLLS.
+     */
+    public void testVarianceBound() {
+        System.out.println("varianceBound");
+        
+        double sigma = 0.0;
+        double[] k = null;
+        AnLLS instance = new AnLLS();
+        
+        double expResult = 0.0;
+        double result = instance.varianceBound(sigma, k);
+        assertEquals(expResult, result);
+        
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
