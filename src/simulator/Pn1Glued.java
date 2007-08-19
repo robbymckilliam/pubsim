df0e5753d096d10b8ce6f87e0bd1a96588423c5b
Robby McKilliam <harprobey@gmail.com>
2007-08-19 23:27:02 +0000

    Had some issues with netbeans and subversion.  Believe that it is fixed now.

--- src/simulator/Pn1Glued.java
+++ src/simulator/Pn1Glued.java
@@ -82,7 +82,7 @@ public class Pn1Glued extends Pn1 implements LatticeNearestPointAlgorithm {
                 System.out.println("u = " + VectorFunctions.print(u));
                  */
             }
-            System.out.println("glue dist = " + dist);
+            //System.out.println("glue dist = " + dist);
             
         }
     }
