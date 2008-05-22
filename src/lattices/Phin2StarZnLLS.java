91bcc36e8570f49045a1699e7a509d25ffd641f7
Robby McKilliam <harprobey@gmail.com>
2008-05-22 02:19:58 +0000

    Refactored Phin2StarZnLLS

--- src/lattices/Phin2StarZnLLS.java
+++ src/lattices/Phin2StarZnLLS.java
@@ -30,37 +30,8 @@ public class Phin2StarZnLLS extends Phin2Star implements NearestPointAlgorithmIn
     @Override
     public void nearestPoint(double[] y){}
     
-    /**
-     * Project a n+2 length vector into Phin2StarZnLLS space.
-     * y is output, x is input (x & y can be the same array)
-     * <p>
-     * PRE: x.length <= y.length
-     */
-    public static void project(double[] x, double[] y){
-        AnstarVaughan.project(x,y);
-        double sumn2 = sumg2(x.length);
-        double nbar = x.length*(x.length + 1)/2 / x.length;
-        double dot = 0.0;
-        for(int i = 0; i < x.length; i++)
-            dot += y[i]*(i+1-nbar);
-        for(int i = 0; i < x.length; i++)
-            y[i] = y[i] - dot/sumn2 * (i+1-nbar);
-    }
-    
     /** {@inheritDoc} */
     @Override
     public double volume(){ return 0;}
-    
-    /** 
-     * Return the magnitude squared of the vector g with n elements.
-     * See Chapter 6 of Robby's confirmation report.
-     */
-    public static double sumg2(int n){
-        double f = Math.floor(n/2.0);
-        double sum = f*(f + 1)*(2*f + 1)/3.0;
-        if(n%2 == 0)
-            sum += n/4.0 - f*(f+1);
-        return sum;
-    }
     
 }
