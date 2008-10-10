f7f075a29cd98490664e70f93662095156bcd518
Robby McKilliam <harprobey@gmail.com>
2008-10-10 03:26:03 +0000

    Added fes.Barankin.  It works, but not as well as I hoped it might.

--- src/simulator/fes/crb/Barankin.java
+++ src/simulator/fes/crb/Barankin.java
@@ -5,8 +5,12 @@
 
 package simulator.fes.crb;
 
+import Jama.Matrix;
+import flanagan.integration.IntegralFunction;
 import flanagan.integration.Integration;
 import java.util.Vector;
+import simulator.Util;
+import simulator.VectorFunctions;
 
 /**
  * Calculates Barankin type bounds for fes.
@@ -18,6 +22,7 @@ public class Barankin extends Hamersley implements BoundCalculator {
     
     /** Constructor.  The amplitude defaults to 1. */
     public Barankin(){
+        testPoints = new Vector<Barankin.TestPoint>();
         setAmplitude(1.0);
     }
     
@@ -35,23 +40,70 @@ public class Barankin extends Hamersley implements BoundCalculator {
      */
     public void setNumberOfTestPoints(int t){
         testPoints.clear();
-        for(int i = 0; i < t; i++){
-            
+        for(int r = 2; r < t+2; r+=2){
+            addTestPoint(1.0/r, -1.0/(2.0*r));
+        }
+        for(int r = 3; r < t+2; r+=2){
+            addTestPoint(1.0/r, 0.0);
         }
     }
     
     @Override
     public double getBound() {
         
-        double dets = N/(ntn*N - nt1*nt1);
         double v = amplitude/Math.sqrt(var);
+        Matrix J = new Matrix(testPoints.size() + 2, testPoints.size() + 2);
         
-        dPoverP func = new dPoverP();
-        func.setv(v);
-        
+        dPOverP func = new dPOverP();
+        func.setv(v);        
         Integration intg = new Integration(func, -0.5, 0.5);
+        double Bint = intg.gaussQuad(100);
         
-        return dets/intg.gaussQuad(1000);
+        /** Set the CRB part of the matrix */
+        J.set(0, 0, ntn * Bint); J.set(0, 1, nt1 * Bint);
+        J.set(1, 0, nt1 * Bint); J.set(1, 1, N * Bint);
+        
+        //now regular/Barankin parts
+        dPPtOverP funcRB = new dPPtOverP();
+        funcRB.setv(v);
+        intg.setIntegrationFunction(funcRB);
+        for(int i = 2; i < J.getRowDimension(); i++){
+            for(int j = 0; j < 2; j++){
+                double sum = 0.0;
+                for(int n = 1; n < N; n++){
+                    double t = testPoints.get(i-2).f * n + testPoints.get(i-2).t;
+                    funcRB.setTranslation(t);
+                    sum += Math.pow(n, 1-j) * intg.gaussQuad(100);
+                }
+                J.set(i, j, -sum);
+                J.set(j, i, -sum);
+            }
+        }
+        
+        //compute the Barankin parts
+        PtPtOverP funcB = new PtPtOverP();
+        funcB.setv(v);
+        intg.setIntegrationFunction(funcB);
+        for(int i = 2; i < J.getRowDimension(); i++){
+            for(int j = i; j < J.getColumnDimension() ; j++){
+                double prod = 1.0;
+                for(int n = 1; n < N; n++){
+                    double t1 = testPoints.get(i-2).f * n + testPoints.get(i-2).t;
+                    double t2 = testPoints.get(j-2).f * n + testPoints.get(j-2).t;     
+                    funcB.setTranslations(t1, t2);
+                    prod *= intg.gaussQuad(100);
+                }
+                J.set(i, j, prod);
+                J.set(j, i, prod);
+            }
+        }
+        
+        //System.out.println(VectorFunctions.print(J));
+        
+        
+        Matrix Jnum = J.getMatrix(1, J.getRowDimension()-1, 1, J.getColumnDimension()-1);
+        
+        return Jnum.det()/J.det();
     }
     
     /** Test point for the Barankin bound */
@@ -61,6 +113,49 @@ public class Barankin extends Hamersley implements BoundCalculator {
             this.f = f;
             this.t = t;
         }
+    }
+    
+    protected class dPPtOverP extends dPOverP implements IntegralFunction {
+        
+        protected double t;
+        
+        public void setTranslation(double t){
+            this.t = t;
+        }
+        
+        @Override
+         public double function(double x) {
+            
+            double fd = dPdf(x, v);
+            double ft = Pdf(x - t, v);
+            double f = Pdf(x, v);
+            
+            return fd*ft/f;
+            
+        }
+        
+    }
+    
+    protected class PtPtOverP extends dPOverP implements IntegralFunction {
+        
+        protected double t1, t2;
+        
+        public void setTranslations(double t1, double t2){
+            this.t1 = t1;
+            this.t2 = t2;
+        }
+        
+        @Override
+         public double function(double x) {
+            
+            double ft1 = Pdf(x-t1, v);
+            double ft2 = Pdf(x - t2, v);
+            double f = Pdf(x, v);
+            
+            return ft1*ft2/f;
+            
+        }
+        
     }
 
 }
