e5bef8304b67d3c57f4253da8588d9d1123e34ae
Robby McKilliam <harprobey@gmail.com>
2010-04-21 22:11:45 +0000

    Minor edits to code.  Added some stuff to draw bounds for polynomial phase estimators.

---
+++ src/simulator/poly/bounds/AngularLeastSquaresVariance.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simulator.poly.bounds;
+
+import distributions.circular.CircularDistribution;
+import distributions.circular.ProjectedNormalDistribution;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Vector;
+
+/**
+ *
+ * @author Robby McKilliam
+ */
+public class AngularLeastSquaresVariance extends BoundCalculator{
+
+    CircularDistribution dist;
+
+    public AngularLeastSquaresVariance(int N, int m, CircularDistribution dist){
+        super(N,m);
+        this.dist = dist;
+    }
+
+    public void setVariance(double var){
+        dist.setVariance(var);
+    }
+
+    @Override
+    public double getBound(int m) {
+        double sigma2 = dist.getWrappedVariance();
+        double d = 1 - dist.pdf(-0.5);
+        return sigma2/(Math.pow(N, 2*m+1)*d*d) * C.get(m,m);
+    }
+
+
+    //** Write CRB data to a file */
+    public static void main(String[] args) throws Exception  {
+
+        int N = 128;
+        int m = 3;
+
+        AngularLeastSquaresVariance bound =
+                new AngularLeastSquaresVariance(N, m, new ProjectedNormalDistribution());
+
+        double from_log_snr = 20.0;
+        double to_log_snr = -1.0;
+        double step_log_snr = -1;
+
+        Vector<Double> snr_array = new Vector<Double>();
+        Vector<Double> snr_db_array = new Vector<Double>();
+        for(double snrdb = from_log_snr; snrdb >= to_log_snr; snrdb += step_log_snr){
+            snr_db_array.add(new Double(snrdb));
+            snr_array.add(new Double(Math.pow(10.0, ((snrdb)/10.0))));
+        }
+
+        for(int j = 0; j <= m; j++){
+            String fname = bound.getClass().getName();
+            fname = fname.concat(Integer.toString(N) + "_p" + j);
+            File file = new File(fname);
+            BufferedWriter writer =  new BufferedWriter(new FileWriter(file));
+            for(int i = 0; i < snr_array.size(); i++){
+                double var = 0.5/snr_array.get(i);
+                bound.setVariance(var);
+                writer.write(
+                        (new Double(var)).toString().replace('E', 'e')
+                        + "\t" + (new Double(bound.getBound(j))).toString().replace('E', 'e'));
+                writer.newLine();
+            }
+            writer.close();
+        }
+
+    }
+
+}
