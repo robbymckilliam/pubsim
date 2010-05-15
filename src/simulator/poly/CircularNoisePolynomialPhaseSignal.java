20b91b534c64e742504e37955f9de295e856f10f
Robby McKilliam <harprobey@gmail.com>
2010-05-15 10:38:04 +0000

    minor edit.

--- src/simulator/poly/CircularNoisePolynomialPhaseSignal.java
+++ src/simulator/poly/CircularNoisePolynomialPhaseSignal.java
@@ -14,17 +14,28 @@ import distributions.circular.CircularDistribution;
  */
 public class CircularNoisePolynomialPhaseSignal extends PolynomialPhaseSignal{
 
+    double[] phasearray = new double[0];
+
+    /**
+     * Returns the phase signal and places the real and imaginary parts
+     * of the complex signal into real and imag arrays that can be retrieved
+     * with getReal() and getImag()
+     * @return
+     */
     @Override
     public double[] generateReceivedSignal() {
+        if(phasearray.length != n) phasearray = new double[n];
+
         for(int t = 0; t < n; t++){
             double phase = 0.0;
             for(int j = 0; j < params.length; j++)
                 phase += Math.pow(t+1,j)*params[j];
             double pnoise = noise.getNoise();
+            phasearray[t] = phase + pnoise;
             real[t] = Math.cos(2*Math.PI*(phase + pnoise));
             imag[t] = Math.sin(2*Math.PI*(phase + pnoise));
         }
-        return null;
+        return phasearray;
     }
 
 }
