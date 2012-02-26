f6e9b573dcda9c3fb2e206081ecb38380946028c
Robby McKilliam <robby.mckilliam@gmail.com>
2012-02-26 13:11:44 +1030

    Added polynomial phase signal ambiguity remover that uses the rectangular identifiable region

---
+++ src/pubsim/poly/AmbiguityRemoverRectangular.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pubsim.poly;
+
+import pubsim.lattices.GeneralLattice;
+import pubsim.lattices.decoder.BabaiNoLLL;
+
+/**
+ *
+ * @author Robby McKilliam
+ */
+public class AmbiguityRemoverRectangular extends AmbiguityRemover {
+
+    public AmbiguityRemoverRectangular(int m) {
+        this.m = m;
+        p = new double[m+1];
+        M = constructBasisMatrix();
+        GeneralLattice lattice = new GeneralLattice(M);
+        sd = new BabaiNoLLL(lattice);
+    }
+}
