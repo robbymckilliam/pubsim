72dbef204bde40d5add83e47077c96425cd91811
Robby McKilliam <robby.mckilliam@gmail.com>
2012-05-15 08:19:36 +0930

    minor edits.

--- src/pubsim/distributions/complex/IndependentRealandImaginary.java
+++ src/pubsim/distributions/complex/IndependentRealandImaginary.java
@@ -19,51 +19,18 @@ public class IndependentRealandImaginary implements ComplexRandomVariable {
 
     final protected RealRandomVariable real;
     final protected RealRandomVariable imag;
-    final protected RealRandomVariable mag;
-    final protected CircularRandomVariable angle;
 
     public IndependentRealandImaginary(RealRandomVariable real, RealRandomVariable imag) {
         this.real = real;
         this.imag = imag;
-
-        mag = new AbstractRealRandomVariable() {
-            public Double getMean() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-            public Double getVariance() {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-            public double pdf(Double x) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        };
-                
-        angle = new CircularRandomVariable() {
-            public double pdf(Double x) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        };  
-        
     }
 
-    @Override
     public RealRandomVariable realMarginal() {
         return real;
     }
 
-    @Override
     public RealRandomVariable imaginaryMarginal() {
         return imag;
-    }
-
-    @Override
-    public RealRandomVariable magnitudeMarginal() {
-        return mag;
-    }
-
-    @Override
-    public RealRandomVariable phaseMarginal() {
-        return angle;
     }
 
     @Override
