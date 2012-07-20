7d3cba4a6c1a802bebc19a0b57cbcea0e55a2d69
Robby McKilliam <robby.mckilliam@gmail.com>
2012-07-20 12:41:59 +1000

    Added stub for barnes wall scripts.

---
+++ scripts/barneswall/run.scala
@@ -0,0 +1,23 @@
+/**
+* Compute Prob Error data
+*/
+import pubsim.lattices.BarnesWall
+
+
+val starttime = (new java.util.Date).getTime
+
+
+//generate some data conjecturing about kissing numbers
+{
+println("Computing probability of error")
+//unfinished.
+println
+}
+
+val runtime = (new java.util.Date).getTime - starttime
+println("Finished in " + (runtime/1000.0) + " seconds.\n") 
+
+
+
+
+
