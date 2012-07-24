571ae934af8c3c33fcbf76ee5cf6da23bce29d9a
Robby McKilliam <robby.mckilliam@gmail.com>
2012-07-24 15:08:41 +1000

    Added more code for VnmCodec

--- src/pubsim/lattices/Vnm/codec/VnmCodec.java
+++ src/pubsim/lattices/Vnm/codec/VnmCodec.java
@@ -43,6 +43,31 @@ public class VnmCodec {
         return 10.0 * Math.log10(secmom/hypercubesecmom);
     }
     
+     /** 
+     * Return the shaping loss with respect to the hypercube for this code.  Value returned in dB.
+     * Modified to model the situation where rate can be sacrificed for shaping gain.
+     */
+    public static double modifiedShapingLoss(int n, int m, int k){
+        if( k >= n) throw new RuntimeException("n must be greater than k, otherwise this code has rate zero!");
+        if(n==1) return 0.0;
+
+        Matrix R = getR(n,m);  
+        double scale = Math.pow(2.0, new Vnm(n, m).logVolume()/n);
+        double secmom = 0.0;
+        for(int i = 0; i < k; i++){
+            double d = Math.abs(R.get(i,i))/scale/2.0;
+            secmom +=d*d*d/12.0;
+        }
+        for(int i = k; i < n; i++){
+            double d = Math.abs(R.get(i,i))/scale;
+            secmom +=d*d*d/12.0;
+        }
+        secmom /= n;
+        //System.out.println(secmom + ", " + scale);
+        double hypercubesecmom = 1.0/12.0;
+        return 10.0 * Math.log10(secmom/hypercubesecmom);
+    }
+    
     /**
      * Get the R part of the QR decomposition of the generator matrix of Vnm.
      * Makes use of Given's rotations and is much faster than the usual QR decomposition.
