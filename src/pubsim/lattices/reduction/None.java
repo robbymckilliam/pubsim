cb7d4a9e156399a09b7e03b1f16d8d97d800d7c7
harprobey <robby.mckilliam@gmail.com>
2013-02-05 14:53:03 +1030

    added null reduction

---
+++ src/pubsim/lattices/reduction/None.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pubsim.lattices.reduction;
+
+import Jama.Matrix;
+
+/**
+ * Identity (or null) reduction, doesn't actually do any reduction
+ * @author Robby McKilliam
+ */
+public class None implements LatticeReduction {
+    
+    Matrix M;
+    
+    @Override
+    public Matrix reduce(Matrix B) {
+        int n = B.getColumnDimension();
+        M = Matrix.identity(n,n);
+        return B;
+    }
+    
+    @Override
+    public Matrix getUnimodularMatrix() {
+        return M;
+    }
+    
+}
