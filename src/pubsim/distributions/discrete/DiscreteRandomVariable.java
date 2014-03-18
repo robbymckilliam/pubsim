cb3b04a4d51062481ba62c48b8aa127ddd506bc5
robbymckilliam <robby.mckilliam@gmail.com>
2014-03-19 10:15:51 +1030

    minor

--- src/pubsim/distributions/discrete/DiscreteRandomVariable.java
+++ src/pubsim/distributions/discrete/DiscreteRandomVariable.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pubsim.distributions.discrete;
 
 import pubsim.distributions.NoiseGenerator;
