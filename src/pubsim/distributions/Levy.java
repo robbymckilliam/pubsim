e27bb5ce435c5f143b889f2b25ed56c71c26e52b
Robby McKilliam <robby.mckilliam@gmail.com>
2014-06-13 09:01:53 +0930

    added Levy noise

--- src/pubsim/distributions/Levy.java
+++ src/pubsim/distributions/Levy.java
@@ -7,20 +7,29 @@ package pubsim.distributions;
  */
 public class Levy extends AbstractRealRandomVariable {
 
-    /** The location parameter */
+    /** The location parameter. */
     public final double mu;
     
-    /** The scale parameter */
+    /** The scale parameter. */
     public final double c;
     
-    /** Construct a Levy distribution with location parameter mu and scale parameter c */
+    /** 
+     * By default the location parameter is zero.
+     * @param c scale parameter 
+     */
+    public Levy(double c){
+        this(0.0,c);
+    }
+    
+    /** Construct a Levy distribution with location parameter mu and scale parameter c. */
     public Levy(double mu, double c){
+        if(c <= 0.0) throw new RuntimeException("Scale parameter c must be greater than 0.");
         this.mu = mu;
         this.c = c;
     }
     
     /** 
-     * The Levy distribution has infinite mean
+     * The Levy distribution has infinite mean.
      * @return  Double.POSITIVE_INFINITY
      */
     @Override
@@ -39,6 +48,8 @@ public class Levy extends AbstractRealRandomVariable {
 
     @Override
     public double pdf(Double x) {
+        double DELTA = c/30;
+        if(x <= mu + DELTA) return 0.0;
         double p1 = Math.sqrt(c/2/Math.PI);
         double p2 = Math.exp(-c/2/(x-mu));
         double p3 = Math.pow(x - mu, 3.0/2.0);
@@ -47,6 +58,7 @@ public class Levy extends AbstractRealRandomVariable {
     
     @Override
     public double cdf(Double x){
+        if(x <= mu) return 0.0;
         double p = Math.sqrt(c/2/(x-mu));
         return pubsim.Util.erfc(p);
     }
